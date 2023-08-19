1195d56eacb912a519ed3e2b55e57885d76201bd
yuboytsun <109614976+yuboytsun@users.noreply.github.com>
2023-08-19 19:09:06 +0300

    Add files via upload

---
+++ movie/movie/src/main/java/com/Movies/movie/Movie.java
@@ -0,0 +1,26 @@
+package com.Movies.movie;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+    @Id
+    private Object _id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+    private List<Review> reviewIds;
+}
---
+++ movie/movie/src/main/java/com/Movies/movie/Review.java
@@ -0,0 +1,18 @@
+package com.Movies.movie;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    @Id
+    private Object id;
+    private String body;
+
+}
